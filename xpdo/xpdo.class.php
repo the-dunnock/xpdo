5189acac5c21b16d358e99db3331145736e22feb
Petri Rautiainen <the_dunnock@outlook.com>
2013-10-19 16:00:35 +0300

    Fix for php 5.6 preg_replace deprecated

--- xpdo/xpdo.class.php
+++ xpdo/xpdo.class.php
@@ -2678,8 +2678,8 @@ class xPDO {
                     }
                     $bound[$pattern] = str_replace(array('\\', '$'), array('\\\\', '\$'), $v);
                 } else {
-                    $parse= create_function('$d,$v,$t', 'return $t > 0 ? $d->quote($v, $t) : \'NULL\';');
-                    $sql= preg_replace("/(\?)/e", '$parse($this,$bindings[$k][\'value\'],$type);', $sql, 1);
+                    preg_match("/(\?)/", $sql, $matches, PREG_OFFSET_CAPTURE);
+                    $sql = substr_replace($sql, $parse($this,$bindings[$k]['value'],$type), $matches[0][1], strlen($bindings[$k]['value']));
                 }
             }
             if (!empty($bound)) {
